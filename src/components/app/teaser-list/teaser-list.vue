f4ac68288bf5d77efc3eb3d00d33424022a51cce
Spike Shape <dev@spike-shape.de>
2018-08-15 21:13:56 +0200

    [TASK] code style for teaser-list example component

--- src/components/app/teaser-list/teaser-list.vue
+++ src/components/app/teaser-list/teaser-list.vue
@@ -1,35 +1,35 @@
 <template>
     <ul>
-        <li v-for="post in posts" v-if="post.title" :key="'id-' + post.id">
-            <icon type="lorem-ipsum" class="icon-lorem-ipsum"></icon>
-            <template v-if="post.href">
-                <a :href="post.href">
-                    <h1 v-if="post.highlight">
-                        {{post.title}}
-                    </h1>
-                    <h2 v-else>{{post.title}}</h2>
-                    <strong>
-                        {{post.summary}}
-                    </strong>
-                    <p>
-                        {{post.content}}
-                    </p>
-                </a>
-            </template>
-            <template v-else>
-                <h1 v-if="post.highlight">
-                    {{post.title}}
-                </h1>
-                <h2 v-else>{{post.title}}</h2>
-                <strong>
-                    {{post.summary}}
-                </strong>
-                <p>
-                    {{post.content}}
-                </p>
-            </template>
-        </li>
-    </ul>
+      <li v-for="post in posts" v-if="post.title" :key="'id-' + post.id">
+        <icon type="lorem-ipsum" viewBox="40 30" class="icon-lorem-ipsum"></icon>
+        <template v-if="post.href">
+          <a :href="post.href">
+            <h1 v-if="post.highlight">
+              {{post.title}}
+            </h1>
+            <h2 v-else>{{post.title}}</h2>
+            <strong>
+              {{post.summary}}
+            </strong>
+            <p>
+              {{post.content}}
+            </p>
+          </a>
+        </template>
+        <template v-else>
+          <h1 v-if="post.highlight">
+            {{post.title}}
+          </h1>
+          <h2 v-else>{{post.title}}</h2>
+          <strong>
+            {{post.summary}}
+          </strong>
+          <p>
+            {{post.content}}
+          </p>
+        </template>
+    </li>
+</ul>
 </template>
 
 <script>
